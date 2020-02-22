356569d9f95ee266e66a6b53344fc2483c7e0fe4
Daniella Starchenko <laliande7@gmail.com>
2020-02-22 17:23:22 +0300

    change return response for route "login"

--- login/api/api.py
+++ login/api/api.py
@@ -4,22 +4,27 @@ from flask import Response
 from flask_bcrypt import Bcrypt, generate_password_hash
 from json import dumps
 from login.registration import Login
-from login.response import *
 
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/login', methods=['POST', 'GET'])
+def createUser():
+    name = request.form.get('name')
+    surname = request.form.get('surname')
+    email = request.form.get('email')
+    login = request.form.get('login')
+    hashedPassword = generate_password_hash(
+        request.form.get('password')).decode('utf-8')
+    user = Login(name, surname, email, login, hashedPassword)
+    user.addRecordToBD()
+
+
+@api.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
-        name = request.form.get('name')
-        surname = request.form.get('surname')
-        email = request.form.get('email')
-        login = request.form.get('login')
-        hashedPassword = generate_password_hash(
-            request.form.get('password')).decode('utf-8')
-        user = Login(name, surname, email, login, hashedPassword)
-        user.addRecordToBD()
-        return Response(dumps(restonse201),  mimetype='application/json')
-    return Response(dumps(restonse405),  mimetype='application/json')
+        try:
+            createUser()
+            return Response(dumps({'response': 'succses create user'}), status=201,  mimetype='application/json')
+        except:
+            return Response(dumps({'response': 'user no created. Pleace, check availability all field'}), mimetype='application/json')
--- login/response.py
+++
@@ -1,3 +0,0 @@
-restonse201 = {'status': 201, 'response': 'Created'}
-restonse405 = {'status': 405,
-               'response': 'Method Not Allowed. Allow method "POST"'}
