afa497a130953a681f924a1be3ed6305f65cb0c3
Daniella Starchenko <laliande7@gmail.com>
2020-02-22 16:34:08 +0300

    create hashed password

--- login/api/api.py
+++ login/api/api.py
@@ -1,21 +1,25 @@
 from flask import Blueprint
 from flask import request
 from flask import Response
+from flask_bcrypt import Bcrypt, generate_password_hash
 from json import dumps
 from login.registration import Login
+from login.response import *
+
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/login', methods=['POST'])
+@api.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         name = request.form.get('name')
         surname = request.form.get('surname')
         email = request.form.get('email')
         login = request.form.get('login')
-        password = request.form.get('password')
-        user = Login(name, surname, email, login, password)
+        hashedPassword = generate_password_hash(
+            request.form.get('password')).decode('utf-8')
+        user = Login(name, surname, email, login, hashedPassword)
         user.addRecordToBD()
-        response = {'status': 201, 'response': 'Created'}
-        return Response(dumps(response),  mimetype='application/json')
+        return Response(dumps(restonse201),  mimetype='application/json')
+    return Response(dumps(restonse405),  mimetype='application/json')
---
+++ login/response.py
@@ -0,0 +1,3 @@
+restonse201 = {'status': 201, 'response': 'Created'}
+restonse405 = {'status': 405,
+               'response': 'Method Not Allowed. Allow method "POST"'}
