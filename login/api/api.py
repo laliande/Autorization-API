7bcbd5734426d65b59924c21c9ee08922eff387b
Daniella Starchenko <laliande7@gmail.com>
2020-02-24 00:07:24 +0300

    add given to token at sign in without check hash password

--- login/api/api.py
+++ login/api/api.py
@@ -1,10 +1,13 @@
 from flask import Blueprint
 from flask import request
 from flask import Response
-from flask_bcrypt import generate_password_hash
+from flask_bcrypt import generate_password_hash, check_password_hash
 from json import dumps
 from login.registration import Login, collection
-
+from datetime import datetime, timedelta
+from time import time
+import jwt
+from config import SECRET_KEY
 
 api = Blueprint('api', __name__)
 
@@ -26,11 +29,30 @@ def CreateUser():
 
 @api.route('/sign-up', methods=['POST'])
 def registration():
-    if request.method == 'POST':
+    try:
+        if (CreateUser()):
+            return Response(dumps({'response': 'sucsess create user'}), status=201,  mimetype='application/json')
+        else:
+            return Response(dumps({'response': 'this email or login are in BD'}), status=404, mimetype='application/json')
+    except:
+        return Response(dumps({'response': 'user no created. Pleace, check availability all field'}), status=400, mimetype='application/json')
+
+
+@api.route('/sign-in', methods=['GET'])
+def signIn():
+    auth = request.authorization
+    if auth:
         try:
-            if (CreateUser()):
-                return Response(dumps({'response': 'sucsess create user'}), status=201,  mimetype='application/json')
-            else:
-                return Response(dumps({'response': 'this email or login are in BD'}), status=404, mimetype='application/json')
+            login = auth.username
+            password = auth.password
+            findUser = list(collection.find(
+                {'login': login, 'password': password}))
+            if len(findUser) != 0:
+                token = jwt.encode(
+                    {'user': login, 'exp': datetime.now() + timedelta(minutes=30)}, SECRET_KEY, algorithm='HS256').decode('utf-8')
+                return Response(dumps({'response': 'TOKEN is ' + token}), status=200, mimetype='application/json')
+            return Response(dumps({'response': 'No valid login or password'}), status=400, mimetype='application/json')
         except:
-            return Response(dumps({'response': 'user no created. Pleace, check availability all field'}), status=400, mimetype='application/json')
+            return Response(dumps({'response': 'No login or password. Pleace, check availability all field'}), status=400, mimetype='application/json')
+    else:
+        return Response(dumps({'response': 'Please, enter authorization data'}), mimetype='application/json')
